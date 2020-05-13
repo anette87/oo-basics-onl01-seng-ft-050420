55ba7dd48fedbf203f3d991d65ed2a6e39c4a83e
Anette Rivera <anettem.rivera@gmail.com>
2020-05-13 03:57:45 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -36,10 +36,20 @@ class Book
     puts "Flipping the page...wow, you read fast!"
     
   end 
-
-
-
-  
 end 
 
+# or 
 
+# class Book
+#   attr_accessor :author, :page_count, :genre
+#   attr_reader :title
+ 
+#   def initialize(title)
+#     @title = title
+#   end
+ 
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+ 
+# end
